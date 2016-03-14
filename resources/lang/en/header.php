7d5abe267da5f205027a375170714d1aa93fc16f
Glenn Hermans <glhermans@gmail.com>
2016-03-14 18:12:57 +0100

    Added keywords for robots

--- resources/lang/en/header.php
+++ resources/lang/en/header.php
@@ -12,6 +12,7 @@ return [
     */
      
     'description' => 'The easiest way for your departments and teams to track how much time they spend on tasks, and get real time results.',
+    'keywords' => 'Time control, Timecontrol, Time management, workforce management, sick leaves, task management, tasks, break management, break, holiday requests, reporting.',    
 
     'home' => 'Home',
     'features' => 'Features',
